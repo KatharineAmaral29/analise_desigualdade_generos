1bc291b6cfd22c97b93a85f83fa9c7e79dcaea3e
Katharine Amaral <katharineamaral@alu.ufc.br>
2019-11-30 10:10:32 -0300

    adicionando slider

--- js/distribuicaoPopulacional.js
+++ js/distribuicaoPopulacional.js
@@ -132,4 +132,38 @@
 	  	.call(colorScale); 
  
     dc.renderAll()
+	
+	
+  // Início Time Slider
+  var dataTime = d3.range(0, 59).map(function(d) {
+    return new Date(1960 + d, 10, 3);
+  });
+
+  var sliderTime = d3
+    .sliderBottom()
+    .min(d3.min(dataTime))
+    .max(d3.max(dataTime))
+    .step(1000 * 60 * 60 * 24 * 365)
+    .width(1040)
+    .tickFormat(d3.timeFormat('%Y'))
+    .tickValues(dataTime)
+    .default(new Date(1998, 10, 3))
+    .on('onchange', val => {
+      d3.select('p#value-time').text(d3.timeFormat('%Y')(val));
+    });
+
+  var gTime = d3
+    .select('div#slider-time')
+    .append('svg')
+    .attr('width', 1110)
+    .attr('height', 100)
+    .append('g')
+    .attr('transform', 'translate(30,30)');
+
+  gTime.call(sliderTime);
+
+  d3.select('p#value-time').text(d3.timeFormat('%Y')(sliderTime.value()));
+
+  //Fim Time Slider
+	
 })();
