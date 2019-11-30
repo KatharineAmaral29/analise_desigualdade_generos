4e76903a8d937d85376097ec7a3e67f6fc688c06
Katharine Amaral <katharineamaral@alu.ufc.br>
2019-11-29 23:15:36 -0300

    estilo mapa novo

--- js/distribuicaoPopulacional.js
+++ js/distribuicaoPopulacional.js
@@ -3,109 +3,133 @@
 	by Katharine Amaral
 */
 
-$(document).ready(function() {
+(async function () {
+
+    var dataset = await d3.csv('https://raw.githubusercontent.com/KatharineAmaral29/analise_desigualdade_generos/master/dados/gender_statistics.csv', function(data){});
 	
-	 	// Instancia um novo mundoMapa
-	 	var osmUrl = "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png",
-	 	    osmAttrib = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, map tiles by &copy; <a href="https://carto.com/attribution">CARTO</a>',
-	 	    osm = L.tileLayer(osmUrl, {
-	 	        maxZoom: 18,
-	 	        attribution: osmAttrib
-	 	    });
-  
-	 	// inicialize o mundoMapa na div "mapid" centralizado no centro do globo e com zoom de 1.5
-	 	var mundoMap = L.map('mapid').setView([16.636192, 13.886719], 1.5).addLayer(osm);
-
-		// Lendo o arquivo com os dados
-		dadosMundiais = d3.csv("https://raw.githubusercontent.com/KatharineAmaral29/analise_desigualdade_generos/master/dados/gender_statistics.csv").then(function(data){
-			let mundoMap = d3.map()
-			data.forEach(function(d) {
-				if(d["Series Code"] == "SP.POP.TOTL.FE.ZS"){
-					if(d["2018"] >= 50){
-						d.maioria = "Feminino"
-					} else if(d["2018"] > 0 && d["2018"] < 50) {
-						d.maioria = "Masculino"
+    var i = 0;
+	var datasetParaMapa;
+	//Criando a variável Maioria para ser plotada no gráfico
+    dataset.forEach(d => {
+		if(d['Series Code'] == 'SP.POP.TOTL.FE.ZS'){ //série correspondente a variável "Population, female (% of total population)" do dataset
+					if(d['2018'] >= 50){
+						d.Maioria = 'Feminino'
+					} else if(d['2018'] > 0 && d['2018'] < 50) {
+						d.Maioria = 'Masculino'
 					} else {
-						d.maioria = "Nenhum"
+						d.Maioria = 'Nenhum'
 					}
-					mundoMap.set(d["Country Name"],[d["2018"], d.maioria])
-					//console.log("Séries " + d["Series Code"] + "Ano 2018 " + d["2018"] + "País " + d["Country Name"] + "Maioria " + d.maioria)
-				}			
-			})
-		})	
-		
-		colorScale = d3.scaleOrdinal()
-          .domain(["Feminino", "Masculino", "Nenhum"])
-          .range(["#EB4CA0", "#6061A6", "#FBF495"]);
-
-		function highlightFeature(e) {
-			let layer = e.target;
-			//console.log(e.target)
-
-			layer.setStyle({
-						weight: 2,
-						color: '#AAA',
-						dashArray: '',
-						fillOpacity: 0.7
-			});
-
-			if (!L.Browser.ie && !L.Browser.opera) {
-				layer.bringToFront();
-			}
-
-			info.update(layer.feature);
+					datasetParaMapa[i].set(d['name_sort'],d.Maioria)
+					i++
 		}
-		
-		var geoj;
+    });
 
-		function resetHighlight(e) {
-			geoj.resetStyle(e.target);
-			info.update();
+	dataset.forEach(d => {
+			if(d['Series Code'] == 'SP.POP.TOTL.FE.ZS'){ //série correspondente a variável "Population, female (% of total population)" do dataset
+			//console.log(d);
 		}
+	});
+	
+	var facts = crossfilter(dataset);
+    var all = facts.groupAll();
+    var countryDim = facts.dimension(d => d['name_sort'])
+    var countryGroup = countryDim.group();
 
-		function zoomToFeature(e) {
-			mundoMap.fitBounds(e.target.getBounds());
-		}
+	/*var factsParaMapa = crossfilter(datasetParaMapa);
+    var allParaMapa = factsParaMapa.groupAll();
+    var countryDimParaMapa = factsParaMapa.dimension(d => d['name_sort'])
+    var countryGroupParaMapa = countryDimParaMapa.group();
+*/
+    let world = await d3.json('https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json');
+    var width = '100%'
+    var height = 600
+    var projection = d3.geoMercator()
 
-		function onEachFeature(feature, layer) {
-			layer.on({
-						mouseover: highlightFeature,
-						mouseout: resetHighlight,
-						click: zoomToFeature
-					});
-		}
-		
-		paises = d3.json("https://raw.githubusercontent.com/AshKyd/geojson-regions/master/countries/110m/all.geojson")
-		
-		$.ajax({
-			type: "GET",
-			url: "https://raw.githubusercontent.com/AshKyd/geojson-regions/master/countries/110m/all.geojson",
-			dataType: 'json',
-			success: function (response) {
-				geojsonLayer = L.geoJson(response, {
-				style: style,
-				onEachFeature: onEachFeature
-		}).addTo(mundoMap);
-				mundoMap.fitBounds(geojsonLayer.getBounds());
-			}
-		});
-		
-		
-		/*
-		geoj = L.geoJson(paises, {
-				style: style,
-				onEachFeature: onEachFeature
-		}).addTo(mundoMap)			
-		*/	
-		
-		function style(feature) {
-			 return {
-						weight: 1,
-						opacity: 1,
-						color: 'white',
-						dashArray: '3',
-						fillOpacity: 0.6,
-						fillColor: "#EB4CA0" //colorScale(dadosMundiais.get(feature.properties.NOME)[1])
-					};
-		}
-});
+    var path = d3.geoPath(projection)
+
+    var zoom = d3.zoom()
+        .scaleExtent([1 / 4, 9])
+        .on('zoom', function () {
+            mapSvg.attr('transform', d3.event.transform)
+        });
+
+	var colorScale = d3.scaleOrdinal()
+			.domain(["Feminino", "Masculino", "Nenhum"])
+			.range(["#EB4CA0", "#6061A6", "#FBF495"]);
+	
+    var features = topojson.feature(world, world.objects.countries1).features;
+
+    var mapSvg = d3.select('#map')
+        .append('svg')
+        .attr('class', 'sea')
+        .attr("width", width)
+        .attr("height", height)
+        .call(zoom)
+        .append('g')
+
+
+    mapSvg.append('g').attr("id", "layer")
+    mapSvg.select('#layer').selectAll("path")
+        .data(features)
+        .enter().append("path")
+		.attr('fill', d => colorScale(dataset.Maioria))
+		.attr('d', path)
+        .on('mouseover', function (d) {
+            d3.select(this)
+                .style('cursor', 'pointer') //muda o mouse para mãozinha
+                .attr('stroke-width', 3)
+                .attr('stroke', '#1AFF00')
+				
+            let coordinates = [0, 0]
+            coordinates = d3.mouse(this)
+            const x = coordinates[0] + 280
+            const y = coordinates[1] + 220
+            showTooltip(dataset['name_sort'], x, y)
+        })
+        .on('mouseout', function (d) {
+            d3.select(this)
+                .style('cursor', 'default')
+                .attr('stroke-width', 0)
+                .attr('stroke', 'none'); //volta ao valor padrão
+            hideTooltip();
+        })
+
+	d3.select("#tooltip").remove()
+		let node = d3.select("body")
+		  .append("div")
+		  .attr("id","tooltip")
+		  .attr("class", "hidden")
+		  .append("p")
+		  .html("<b>País <u>X</u><span id='country_name'></b></span><br/><span id='taxa'> </span><u><b>Y</b></u>% da população total é mulher.")
+
+	function showTooltip(county_id, x, y){
+		d3.select("#tooltip")
+		.style("left", x + "px")
+		.style("top", y + "px")
+		.select("#taxa")
+		.text(dataset['Maioria']);
+	  
+		d3.select("#tooltip")
+		.select("#country_name")
+		.text(dataset['name_sort']);
+	  
+		d3.select("#tooltip")
+		.classed("hidden", false)
+	}
+	
+	function hideTooltip(){
+		d3.select("#tooltip")
+		.classed("hidden", true)
+	}
+	
+	//Legenda de cores   
+	
+	mapSvg.append("g")
+      .attr("class", "legendOrdinal")
+      .attr("transform", "translate(20,20)");
+	
+    mapSvg.select(".legendOrdinal")
+	  	.call(colorScale); 
+ 
+    dc.renderAll()
+})();
